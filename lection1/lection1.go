1235b0948c6b31b73b7e827f9901cdf00b120609
Tetiana Tymoshchuk <teti.tym@gmail.com>
2022-06-14 19:02:27 +0200

    add file lection1.go

--- lection1/lection1.go
+++ lection1/lection1.go
@@ -3,4 +3,14 @@ package main
 func main() {
 	println("Слава Україні!")
 	println("Героям слава!")
+	println("Слава Україні!")
+	println("Героям слава!")
+
+	var i int = 1000000
+	var i1 int = 1_000_000
+
+	println(i, i1)
+	i++
+	// println(i++)
+	println(i)
 }
