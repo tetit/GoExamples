7dc546ecd94c70b4db872a9fda5cc77b13333a74
Tetiana Tymoshchuk <teti.tym@gmail.com>
2022-06-15 13:05:20 +0200

    modified:   lection1.go

--- lection1/lection1.go
+++ lection1/lection1.go
@@ -1,10 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 func main() {
 	println("Слава Україні!")
 	println("Героям слава!")
 	println("Слава Україні!")
-	println("Героям слава!")
+	println(`Героям слава!%"`)
+
+	s := "Some text"
+	s1 := strings.ToLower(s)
+	fmt.Println(s1)
 
 	var i int = 1000000
 	var i1 int = 1_000_000
